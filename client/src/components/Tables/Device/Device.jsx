eca91c7114d140f44044f0ace56b93741d1f9d77
Cristian <cristian.niculescu@gmail.com>
2021-11-07 23:00:11 -0800

    generate table .map function with checkmark/cross icons

--- client/src/components/Tables/Device/Device.jsx
+++ client/src/components/Tables/Device/Device.jsx
@@ -1,50 +1,81 @@
 import React, { Component } from "react";
-import { Button, Divider, Icon, Table } from 'semantic-ui-react';
+import { Button, Divider, Icon, Table, TransitionablePortal } from 'semantic-ui-react';
 
 // const colors = [ "red", "green" ]
 
 class DeviceTable extends Component {
 
   state = {
+    deviceNameInfo: [["awst0", "awst1"], ["true", "false"]],  // can this be passed 
+    deviceNameInfo2: [{ device: "awst0", cleaning: true },
+                      { device: "awst1", cleaning: false }]
   }
+
+  // componentDidMount() {
+  //   this.formDeviceNamesInfoObj()
+  // }
+
+  // formDeviceNamesInfoObj() {
+  //   const deviceNamesInfo = []
+  //   for (let i = 0; i < this.props.DeviceNames.length; i++) {
+  //     const output = {
+  //       device: this.props.DeviceNames[i],
+  //       cleaning: this.props.DeviceInfo[i]
+  //     }
+  //     deviceNamesInfo.push(output)
+  //   }
+  //   // console.log("obj", deviceNamesInfo)
+  //   return deviceNamesInfo
+  // }
 
   render() {
     
-    const { SrvInfo, DeviceNames, DeviceInfo } = this.props
-    console.log("DeviceTable Component - DeviceInfo:", DeviceInfo)
+    const { SrvInfo, DeviceNames, DeviceInfo, DeviceNamesInfo,
+      DeviceNamesInfo2, DeviceNamesInfoNew, workingArray } = this.props
+    // console.log("DeviceTable Component - DeviceInfo:", DeviceInfo)
+    // console.log("DeviceTable Component - DeviceNamesInfo:", DeviceNamesInfo[0])
+    
+    // console.log("Table - DeviceNamesInfo2Dash", DeviceNamesInfo2Dash)
+    // console.log("Table - DeviceNamesInfoNew", DeviceNamesInfoNew)
+
+    // console.log("=>=> obj", this.formDeviceNamesInfoObj())
+    // console.log("=> obj-1", this.formDeviceNamesInfoObj().map(device => device[0]))
+    // console.log("device 1", this.formDeviceNamesInfoObj()[0])
+    // console.log("device 1 - device", this.formDeviceNamesInfoObj().map(devices => devices.device)[0])
+    // console.log("device 1 - clean", this.formDeviceNamesInfoObj().map(clean => clean.cleaning))
+
+    // console.log("deviceNameInfo", this.state.deviceNameInfo)
+    // console.log("deviceNameInfo", this.state.deviceNameInfo)
+    // console.log("NameInfo",this.state.deviceNameInfo.map(deviceNameInfo => deviceNameInfo[0]))  // ['awst0', 'true'] 
+    // console.log("=>=>test", this.state.deviceNameInfo.map(deviceNameInfo => deviceNameInfo[0])[0]) // awst0
       
     return (
-      <section className="devices">
-        {/* <h3>Hostname: {SrvInfo.hostname}</h3> */}
-        <div className="devices__table-wrapper">
+      <section className="device">
+        <h3 className="device__heading">P5 Device Check</h3>
+        {/* <h3 className="devices__hostname">Hostname: {SrvInfo.hostname}</h3> */}
+        <div className="device__table-wrapper">
           <Table compact>
             <Table.Header>
-              <Table.Row>
+              {/* <Table.Row>
                 <Table.HeaderCell colSpan='2'>Host: {SrvInfo.hostname}</Table.HeaderCell>
-              </Table.Row>
+              </Table.Row> */}
             </Table.Header>
             <Table.Header>
               <Table.Row>
                 <Table.HeaderCell>Device</Table.HeaderCell>
-                <Table.HeaderCell>Needs Cleaning</Table.HeaderCell>
+                {/* <Table.HeaderCell>Needs Cleaning</Table.HeaderCell> */}
+                <Table.HeaderCell>Cleaning Required</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
             <Table.Body>
-              <Table.Row>
-                <Table.Cell>{DeviceNames[0]}</Table.Cell>
-                <Table.Cell positive>
-                  <Icon name='checkmark' />
-                    {DeviceInfo[0]}
-                </Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>{DeviceNames[1]}</Table.Cell>
-                <Table.Cell negative>
-                  <Icon name='close' />
-                    {DeviceInfo[1]}
-                </Table.Cell>
-              </Table.Row>
-            <Table.Row></Table.Row>
+              {DeviceNamesInfo.map(device =>
+                <Table.Row>
+                  <Table.Cell>{device.device}</Table.Cell>                 
+                  {device.cleaning === true ?
+                    (<Table.Cell positive><Icon name='checkmark' />{device.cleaning.toString()}</Table.Cell>)
+                  : (<Table.Cell negative><Icon name='close'/>{device.cleaning.toString()}</Table.Cell>)}
+                </Table.Row>                  
+              )}
             </Table.Body>
           </Table>
         </div>
@@ -53,25 +84,9 @@ class DeviceTable extends Component {
   }
 }
 
-
 export default DeviceTable
 
 
 
-  // handleClick(letter) {
-  //   this.setState({ justClicked: letter });
-  // }
-  // render() {
-  //   return (
-  //     <div>
-  //       Just clicked: {this.state.justClicked}
-  //       <ul>
-  //         {this.state.letters.map(letter =>
-  //           <li key={letter} onClick={() => this.handleClick(letter)}>
-  //             {letter}
-  //           </li>
-  //         )}
-  //       </ul>
-  //     </div>
 
 
