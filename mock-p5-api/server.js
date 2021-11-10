4f705434a7f15d9241033ada19e72315fcd93287
Cristian <cristian.niculescu@gmail.com>
2021-11-10 15:09:11 -0800

    CORS configured with origin: true to fix the CORS error by adding the 'Access-Control-Allow-Origin' header on the requested resource. Remove CORS if this api ends up in production. Also a very specific url origin can be set

--- mock-p5-api/server.js
+++ mock-p5-api/server.js
@@ -1,13 +1,59 @@
 const express = require("express");
 const app = express();
+const helmet = require("helmet")
 const morgan = require("morgan");
+const logger = require("morgan");
 const cors = require("cors");
+const basicAuth = require("express-basic-auth");
 require("dotenv").config();
+
 const PORT = process.env.PORT || 8080;
 
+app.use(helmet());  
 app.use(morgan("dev"));
-app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
+app.use(cors());
+app.use(
+  cors({
+    origin: true,
+		credentials: true,  // prompts for username and password
+	})
+);
+
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
+
+const users = {}
+users[username] = password
+
+app.use(
+	basicAuth({
+		users: users, // { username: password }
+		challenge: true,
+		unauthorizedResponse: getUnauthorizedResponse,
+	})
+);
+function getUnauthorizedResponse(req) {
+	return req.auth
+		? "Credentials " + req.auth.user + ":" + req.auth.password + " rejected"
+		: "No credentials provided";
+}
+
+//------- for testing ------//
+app.get("/base64auth", (req, res) => {
+  // console.log("request", req);
+	res.json({
+		Welcome: "Welcome to the Base64 encrypted page. You made it =)",
+		requestHeaders: req.headers,
+		auth: req.auth,
+		body: req.body,
+		route: req.route,
+		params: req.params,
+		query: req.query,
+		url: req.url,
+	});
+});
 
 const homepage = require("./routes/homepage");
 const license = require("./routes/license");
@@ -16,12 +62,7 @@ const clients = require("./routes/general/clients");
 const plans = require("./routes/archive/plans");
 const indexes = require("./routes/archive/indexes");
 const devices = require("./routes/archive/devices");
-
-app.get("/", (req, res) => {
-	res.json({
-		Welcome: "Welcome to the Archiware P5 Mock API",
-	});
-});
+// const { get } = require("./routes/homepage");
 
 app.get("/", homepage);
 app.use("/general", general);
@@ -31,19 +72,15 @@ app.use("/archive/indexes", indexes);
 app.use("/general/devices", devices);
 app.use("/general/clients", clients);
 
+app.get("/", (req, res) => {
+	res.json({
+		Welcome: "Welcome to the Archiware P5 Mock API",
+	});
+});
+
 app.listen(PORT, console.log(`Server listening at: http://localhost:${PORT}`));
 
+console.log("username", username);
+console.log("password", password);
+console.log("users", users);
 
-
-
-
-
-
-
-
-// --- Try out chokidar --- //
-// const chokidar = require("chokidar");
-// // One-liner for current directory
-// chokidar.watch(".").on("all", (event, path) => {
-// 	console.log(event, path);
-// });
