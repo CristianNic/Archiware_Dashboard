28a529d0c935508331c49fdfd160a6d7b1b4b923
Cristian <cristian.niculescu@gmail.com>
2021-11-10 15:12:23 -0800

    function to generate random numbers to simulate LicenseResource Info. -1 represents unlimted free, 0 no free licenses, a positive number is the number of free licenses

--- mock-p5-api/routes/license.js
+++ mock-p5-api/routes/license.js
@@ -6,6 +6,10 @@ function loadLicenseResourceData() {
 	return fs.readFileSync("./data/license/get-license-resource-names.json", "utf8");
 }
 
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 // license.get("/", (req, res) => {
 // 	res.json({
 // 		Welcome: "Welcome to /license",
@@ -17,5 +21,27 @@ license.get("/", (req, res) => {
 	const licenseResourceNames = JSON.parse(loadLicenseResourceData());
 	res.json(licenseResourceNames);
 });
+
+// GET LicenseResourceInfo --> {{baseUrl}}/:resourceID
+license.get("/:resourceID", (req, res) => {
+	console.log(":resourceID", req.params);
+  const rndInt = randomIntFromInterval(-1, 3)
+  console.log(rndInt)
+  res.json({ licenses: rndInt });
+
+	// if (req.params.resourceID === "ArchivePlan") {
+	// 	res.json({ licenses: -1 });
+	// } else if (req.params.resourceID === "BackupPlan") {
+	// 	res.json({ licenses: 0 });
+	// } else {
+	// 	res.json({ Required: "no matching :deviceID" });
+	// }
+});
+
+  // const rndInt = this.randomIntFromInterval(-1, 3)
+  // console.log(rndInt)
+
+
+
 
 module.exports = license;
