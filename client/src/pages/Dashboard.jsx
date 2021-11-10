d3a115f6716fd7797d828a0d07096abfdbea6597
Cristian <cristian.niculescu@gmail.com>
2021-11-10 15:01:00 -0800

    API_URL and axios auth config moved to utils/Auth. Device and License Resource calls moved to their components. Moving the rest is next.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,87 +1,43 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import {
-  Button, Divider, Table,
-} from 'semantic-ui-react';
+import { API_URL, auth } from '../utils/Auth';
+import { Divider } from 'semantic-ui-react';
 // import Timer from '../components/Timer/Timer'
 // import CountDownTimer from '../components/CountDownTimer/CountDownTimer'
 // const hoursMinSecs = { hours: 0, minutes: 0, seconds: 7 }
-import Refresh from "../components/Refresh/Refresh";
+// import Refresh from "../components/Refresh/Refresh";
 import SrvInfoTable from "../components/Tables/SrvInfo/SrvInfo";
 import DeviceTable from "../components/Tables/Device/Device";
-
-const API_URL = "http://localhost:8080"
+import LicenseResourceTable from "../components/Tables/LicenseResource/LicenseResource";
 
 class Dashboard extends Component {
   
   state = {
     HomePage: [],
-    SrvInfo: [],
-    LicenseResourceNames: [],
     ArchivePlanNames: [],
     ArchiveIndexNames: [],
-    DeviceNames: [], 
-    DeviceInfo: [],
-    DeviceNamesInfo: [],
     ClientNames: [],  
     ClientInfo: [],
     isLoaded: true,
-    workingArray: []
   }
 
   componentDidMount() {
-    this.getServerInfo()
-    this.getLicenseResourceNames()
-    this.getArchivePlanNames()
-    this.getArchiveIndexNames()
-    // this.getDeviceNames()
-    this.getClientNames()
-
-    this.getDeviceNamesInfo()
+    this.getHomePage()
   }
   
   componentDidUpdate() {
-    this.formDeviceNamesInfo() 
   }
 
   getHomePage() {  
     axios
-      .get(`${API_URL}/`)
+      .get(`${API_URL}/`, auth)
       .then((response) => {
-        // console.log(response)
+        console.log("getHomePage()", response)
         this.setState({
           HomePage: response.data.HomePage,
         })
       })
   }  
-
-  getServerInfo() {
-    axios
-      .get(`${API_URL}/general/srvinfo`)
-      .then((response) => {
-        // console.log(response)
-        this.setState({
-          SrvInfo: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log('error:', error.response.data);
-      })
-  }
-
-  getLicenseResourceNames() {  
-    axios
-      .get(`${API_URL}/license/resources`)
-      .then((response) => {
-        // console.log(response)
-        this.setState({
-          LicenseResourceNames: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log('error:', error.response.data);
-      })
-  }
 
   getArchivePlanNames() {  
     axios
@@ -109,71 +65,6 @@ class Dashboard extends Component {
       .catch((error) => {
         console.log('error:', error.response.data);
       })
-  }
-
-  getDeviceNames() {  
-    axios
-      .get(`${API_URL}/general/devices`)
-      .then((response) => {
-        // console.log("response:", response.data)
-        const devices = response.data.map(dev => dev.devices[0].ID)
-        // const id = devices.map(dev => dev.devices)
-        this.setState({
-          DeviceNames: devices,
-        })
-      })
-      .catch((error) => {
-        console.log('error:', error.response.data);
-      })
-  }
-
-  getDeviceNamesInfo() {
-    axios.get(`${API_URL}/general/devices`)
-      .then((response) => {
-        const devices = response.data.map(dev => dev.devices[0].ID)
-        const promises = []
-        devices.forEach((device) => {
-          promises.push(axios.get(`${API_URL}/general/devices/${device}`))
-        })
-        return Promise.all(promises).then((response) => {
-          const info = response.map(deviceInfo => deviceInfo.data.cleaning)
-          const devicesInfoArray = []
-          for (let i = 0; i < devices.length; i++) {
-            const obj = {
-              device: devices[i],
-              cleaning: info[i]
-            }
-            devicesInfoArray.push(obj)
-          }
-          this.setState({
-            DeviceNamesInfo: devicesInfoArray
-          })
-        });
-      })
-  }
-
-  getDeviceInfo(deviceID) {
-    // Better if it loops through the drives, makes a call for each, populating an array
-    axios
-      .get(`${API_URL}/general/devices/${deviceID}`) // awst0 or awst1
-      .then((response) => {
-      console.log(response)
-        this.setState({
-          DeviceInfo: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log('error:', error.response.data);
-      })
-  }
-
-  formDeviceNamesInfo(getDeviceInfo) {
-    // const devices = this.state.DeviceNames
-    // console.log("devices", devices)
-    // devices.forEach(device => console.log(device))
-  //  devices.forEach(deviceID => getDeviceInfo(deviceID))
-    // getDeviceInfo("awst0")
-    // console.log("devices:", devices)
   }
 
   getClientNames() {
@@ -220,35 +111,13 @@ class Dashboard extends Component {
 
   refreshData() {
     console.log("onClick Button - Refreshed Data")
-    this.getServerInfo()
-    this.getLicenseResourceNames()
-    this.getArchivePlanNames()
-    this.getArchiveIndexNames()
-    this.getDeviceNames()
-    this.getClientNames()
+    // reload page 
   }
   
   render() {
 
-    // console.log("this.state.DeviceInfo",  this.state.DeviceInfo)
-    // console.log("this.state.DeviceNames",  this.state.DeviceNames)
-    // console.log("this.state.DeviceNamesInfo",  this.state.DeviceNamesInfo)
-
-    // console.log("Async Get Devices", this.getDevices())
-    // console.log("HomePage:",              this.state.HomePage)
-    // console.log("SrvInfo:",               this.state.SrvInfo)
-    // console.log("LicenseResourceNames:",  this.state.LicenseResourceNames)
-    // console.log("ClientNames:",           this.state.LicenseResourceNames.resources)
-    // console.log("ArchivePlanNames:",      this.state.ArchivePlanNames)
-    // console.log("ArchiveIndexNames:",     this.state.ArchiveIndexNames)
-    // console.log("DeviceNames:",           this.state.DeviceNames)
-    // // this.getDeviceInfo("awst0")
-    // // this.getDeviceInfo("awst1")
-    // console.log("DeviceInfo - 'awst1':",  this.state.DeviceInfo)
-    // console.log("ClientNames:",           this.state.ClientNames)
-    // // this.getClientInfo("jellyfish")
-    // console.log("ClientInfo 'jellyfish':", this.state.ClientInfo)
-
+    console.log("Dashboard - API_URL", API_URL)
+    
     return (
       <section className="dashboard">
         <div className="dashboard__container">    
@@ -256,19 +125,21 @@ class Dashboard extends Component {
             className="dashboard__refresh"
             refreshData={this.refreshData}/> */}
           <Divider/>
-          
           <SrvInfoTable
             className="dashboard__srvinfo"
-            SrvInfo={this.state.SrvInfo} />
+            SrvInfo={this.state.SrvInfo}
+          />
           <Divider/>
-
           <DeviceTable
             className="dashboard__devices"
-            SrvInfo={this.state.SrvInfo}
             DeviceNames={this.state.DeviceNames}
             DeviceInfo={this.state.DeviceInfo}
-            DeviceNamesInfo={this.state.DeviceNamesInfo}/>
+          />
           <Divider/>
+          <LicenseResourceTable
+            className="dashboard__devices" />
+          <Divider/>
+          
         </div>
       </section>
     )
