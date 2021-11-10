37a72553a1d3ca9b0ced3f659104e323e9eb988f
Cristian <cristian.niculescu@gmail.com>
2021-11-10 14:21:02 -0800

    added page to test server response to base64 auth requests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // Redirect
 import Dashboard from "./pages/Dashboard";
 // import Controls_Sandbox from "./components/Controls_Sandbox/Controls_Sandbox";
+import Settings from "./components/Settings/Settings";
 
 class App extends Component {
 	render() {
@@ -9,6 +10,7 @@ class App extends Component {
 			<Router>
 				<Switch>
 					<Route exact path="/" component={Dashboard} />
+					<Route exact path="/base64auth" component={Settings} />
 					{/* <Route exact path="/controls_sandbox" component={Controls_Sandbox} /> */}
 				</Switch>
 			</Router>
