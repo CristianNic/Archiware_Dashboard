560a54d1727687fed7e7641f42fb98c3f953224a
Cristian <cristian.niculescu@gmail.com>
2021-11-16 21:07:56 -0800

    add dashboard mockup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // Redirect
 import Dashboard from "./pages/Dashboard";
+import Dashboardv2 from "./pages/Dashboardv2";
 // import Controls_Sandbox from "./components/Controls_Sandbox/Controls_Sandbox";
 import Settings from "./components/Settings/Settings";
 
@@ -10,7 +11,9 @@ class App extends Component {
 			<Router>
 				<Switch>
 					<Route exact path="/" component={Dashboard} />
-					<Route exact path="/base64auth" component={Settings} />
+					<Route exact path="/v2" component={Dashboardv2} />
+          <Route exact path="/base64auth" component={Settings} />
+          {/* <Route exact path="/base64auth" element={<Settings API_URL="www.sweet-test.com"/>} /> */}
 					{/* <Route exact path="/controls_sandbox" component={Controls_Sandbox} /> */}
 				</Switch>
 			</Router>
