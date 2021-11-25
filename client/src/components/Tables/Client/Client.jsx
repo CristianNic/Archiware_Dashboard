637b73e4c40c06a7b9902c9e9343e6fb3ff25247
Cristian <cristian.niculescu@gmail.com>
2021-11-24 23:11:06 -0800

    completed client table

--- client/src/components/Tables/Client/Client.jsx
+++ client/src/components/Tables/Client/Client.jsx
@@ -1,98 +1,113 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import { API_URL, auth } from '../../../utils/Auth';
+import { API_URL } from '../../../utils/Auth';
 import { Table } from 'semantic-ui-react';
-
-// const options = {
-//   headers: { Authorization: "Basic Y3Jpc3RpYW46bXVua2lyZXBvcnQgbXVua2k=" }
-// }
   
 class Client extends Component {
 
   state = {
-    // SrvInfo: [],
-    // Uptime: []
+    clientTable: []
   }
 
   componentDidMount() {
-    this.getServerInfo()
-    // this.getServerInfoHeaders()
+    this.getClientNamesInfo()
   }
 
-  getServerInfo() {
-    // axios({headers: { Authorization: "Basic Y3Jpc3RpYW46bXVua2lyZXBvcnQgbXVua2k=" }})
-    axios
-      // .get(`${API_URL}/general/srvinfo`, { headers: { Authentication: "Basic Y3Jpc3RpYW46bXVua2lyZXBvcnQgbXVua2k="} }  )
-      .get(`${API_URL}/general/srvinfo`, auth)
-      .then((response) => {
-        // console.log("SrvInfo", response)
-        // console.log("Uptime", response.data.uptime)
-        const uptime = new Date(response.data.uptime * 1000).toISOString().substr(11, 8)
-        // console.log("Uptime", uptime)
-        this.setState({
-          SrvInfo: response.data,
-          Uptime: uptime
-        })
-      })
-      .catch((error) => {
-        // console.log('error:', error.response.data);
-        console.log('error:', error);
-      })
+  async getClientNamesInfo() {
+
+    const getClientNames = await axios.get(`${API_URL}/general/clients`)
+
+    const clientNames = getClientNames.data.clients.map(client => client.ID) // ['Synology', 'localhost', ...]
+
+    const getClientInfoPromises = []
+    clientNames.forEach(clientID => {
+      getClientInfoPromises.push(axios.get(`${API_URL}/general/clients/${clientID}`))
+    })
+    const clientInfo = await Promise.all(getClientInfoPromises)
+
+    const description = clientInfo.map(desc => desc.data.description)
+    const hostname = clientInfo.map(hostname => hostname.data.hostname)
+    const port = clientInfo.map(port => port.data.port)
+    const username = clientInfo.map(username => username.data.username)
+    const isThin = clientInfo.map(thin => thin.data.isthin)
+    // Returns true in case the client is of type Workstation (as opposed to type Server)
+    const isThinFormatted = []
+    isThin.forEach(station => {
+      if (station === true) {
+        isThinFormatted.push("Workstation")
+      } else {
+        isThinFormatted.push("Server")
+      }
+    })
+
+    // ===> Form DataTable Array 
+    const clientTable = []
+    for (let i = 0; i < clientInfo.length; i++) {
+      const obj = {
+        clientID: clientNames[i],
+        description: description[i],
+        hostname: hostname[i],
+        isThin: isThinFormatted[i],
+        port: port[i],
+        username: username[i]
+      }
+      clientTable.push(obj)
+    }
+    // console.log('ClientTable:', clientTable)
+
+    this.setState({
+      clientTable: clientTable
+    })
   }
-  
+
   render() {
 
-    // const { SrvInfo, Uptime } = this.state
-      
+    const { clientTable } = this.state
+    
     return (
       <section className="client">
-        {/* <h3 className="srvinfo__heading">P5 Server Info</h3> */}
         <h3 className="client__heading">Client Info</h3>
         <div className="client__table-wrapper">
-          <Table compact>
-            <Table.Header>
-              {/* <Table.Row>
-                <Table.HeaderCell colSpan='8'>/general/srvinfo</Table.HeaderCell>
-              </Table.Row> */}
-            </Table.Header>
+          <Table compact celled>
             <Table.Header>
               <Table.Row>
-                {/* GET ClientNames */}
                 <Table.HeaderCell>Client</Table.HeaderCell>
-                {/* Get Client Info */}
                 <Table.HeaderCell>Description</Table.HeaderCell>
                 <Table.HeaderCell>Hostname</Table.HeaderCell>
                 <Table.HeaderCell>Port</Table.HeaderCell>
+                <Table.HeaderCell>Type</Table.HeaderCell>
                 <Table.HeaderCell>Username</Table.HeaderCell>
-                <Table.HeaderCell>IsThin</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
             <Table.Body>
-              <Table.Row>
-                <Table.Cell>Synology</Table.Cell>
-                <Table.Cell>empty</Table.Cell>
-                <Table.Cell>192.168.234.142</Table.Cell>
-                <Table.Cell>8060</Table.Cell>
-                <Table.Cell>P5_user</Table.Cell>
-                <Table.Cell>false</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>localhost</Table.Cell>
-                <Table.Cell>something</Table.Cell>
-                <Table.Cell>192.168.234.143</Table.Cell>
-                <Table.Cell>8080</Table.Cell>
-                <Table.Cell>A_user</Table.Cell>
-                <Table.Cell>true</Table.Cell>
-              </Table.Row>
+              {Object.keys(clientTable).length === 0 ?
+                (<Table.Row>
+                  <Table.Cell>loading...</Table.Cell>
+                  <Table.Cell>loading...</Table.Cell>
+                  <Table.Cell>loading...</Table.Cell>
+                  <Table.Cell>loading...</Table.Cell>
+                  <Table.Cell>loading...</Table.Cell>
+                  <Table.Cell>loading...</Table.Cell>
+                </Table.Row>)
+                :
+                (clientTable.map(client => 
+                  <Table.Row>
+                    <Table.Cell>{client.clientID}</Table.Cell>
+                    <Table.Cell>{client.description}</Table.Cell>
+                    <Table.Cell>{client.hostname}</Table.Cell>
+                    <Table.Cell>{client.port}</Table.Cell>
+                    <Table.Cell>{client.isThin}</Table.Cell>
+                    <Table.Cell>{client.username}</Table.Cell>
+                  </Table.Row>)
+                )   
+              }
             </Table.Body>
           </Table>
           {/* <br /> */}
-          
           {/* <div className="client__buttons">
             <Button className="client__button">Update Client Password</Button>
             <Button className="client__button">Ping Client</Button>
           </div> */}
-
         </div>
       </section>
     )
