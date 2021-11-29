edd74a39fbfdf2f9894d3730fd547e17b55aa7e4
Cristian <cristian.niculescu@gmail.com>
2021-11-28 20:55:25 -0800

    Added sidebar and tabs, only one tab is active at a time, and currently the client table is in it.

--- client/src/pages/Dashboardv2.jsx
+++ client/src/pages/Dashboardv2.jsx
@@ -2,132 +2,125 @@ import React, { Component } from "react";
 import axios from 'axios';
 import { API_URL, auth } from '../utils/Auth';
 import { Divider } from 'semantic-ui-react';
-// import { Tab, Menu } from 'semantic-ui-react';
-// import Timer from '../components/Timer/Timer'
-// import CountDownTimer from '../components/CountDownTimer/CountDownTimer'
-// const hoursMinSecs = { hours: 0, minutes: 0, seconds: 7 }
-// import Refresh from "../components/Refresh/Refresh";
-import SrvInfo from "../components/Tables/SrvInfo/SrvInfo";
-import Devices from "../components/Tables/Devices/Devices";
-import LicenseResources from "../components/Tables/LicenseResources/LicenseResources";
-import Jukebox from "../components/Tables/Jukebox/Jukebox";
-import Pool from "../components/Tables/Pool/Pool";
-import Jobs from "../components/Tables/Jobs/Jobs";
+import { Tab, Menu, Segment, Icon, Button } from 'semantic-ui-react';
+// import SrvInfo from "../components/Tables/SrvInfo/SrvInfo";
+// import LicenseResources from "../components/Tables/LicenseResources/LicenseResources";
+// import Devices from "../components/Tables/Devices/Devices";
+import Client from "../components/Tables/Client/Client";
+// import Jukebox from "../components/Tables/Jukebox/Jukebox";
+// import Pool from "../components/Tables/Pool/Pool";
+// import Jobs from "../components/Tables/Jobs/Jobs";
+import MenuHeading from "../components/Menu/Menu"
+import Tabs from "../components/Tabs/Tabs"
+import Sidebar from "../components/Sidebar/Sidebar"
 
-// const USERNAME = process.env.REACT_APP_USERNAME;
-// const PASSWORD = process.env.REACT_APP_PASSWORD;
+// activeServer // displayedServer  
+
+// const second = "===> Second URL"
 
 class Dashboard extends Component {
   
   state = {
-    HomePage: [],
-    ArchivePlanNames: [],
-    ArchiveIndexNames: [],
-    ClientNames: [],  
-    ClientInfo: [],
     isLoaded: true,
+    activeServer: 'BackupServerMini',  
+    // url: "===> First URL"
   }
 
   componentDidMount() {
-    // this.getHomePage()
-  }
-  
-  componentDidUpdate() {
   }
 
-  getHomePage() {  
-    axios
-      .get(`${API_URL}/`, auth)
-      .then((response) => {
-        console.log("getHomePage()", response)
-        this.setState({
-          HomePage: response.data.HomePage,
-        })
-      })
-  }  
-
-  getArchivePlanNames() {  
-    axios
-      .get(`${API_URL}/archive/plans`)
-      .then((response) => {
-        // console.log(response)
-        this.setState({
-          ArchivePlanNames: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log('error:', error.response.data);
-      })
-  }
-
-  getArchiveIndexNames() {  
-    axios
-      .get(`${API_URL}/archive/indexes`)
-      .then((response) => {
-        // console.log("response", response.data)
-        this.setState({
-          ArchiveIndexNames: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log('error:', error.response.data);
-      })
-  }
-
-  getClientNames() {
-    axios
-      .get(`${API_URL}/general/clients`)
-      .then((response) => {
-      //  console.log(response)
-        this.setState({
-          ClientNames: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log('error:', error.response.data);
-      })
-  }
-
-  getClientInfo(clientID) {
-    axios
-      .get(`${API_URL}/general/clients/${clientID}`)  // jellyfish
-      .then((response) => {
-      //  console.log(response)
-        this.setState({
-          ClientInfo: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log('error:', error.response.data);
-      })
-  }
-
-  // countDown() {
-  //   // Remove one second, set state so a re-render happens.
-  //   const seconds = this.state.seconds - 1;
+  // selectServerClick = (e, { name }) =>
   //   this.setState({
-  //     time: this.secondsToTime(seconds),
-  //     seconds: seconds,
-  //   });
-    
-  //   // Check if we're at zero.
-  //   if (seconds == 0) { 
-  //     clearInterval(this.timer);
-  //   }
-  // }
+  //     activeServer: name,
+  //     // url: firstOne
+  //   })
+  
+  selectServerClick = (e, { name }) => {
+
+    if (name === "BackupServerMini") {
+      this.setState({
+        activeServer: name,
+        // url: "===> First URL"
+      })
+      this.forceUpdate()
+    } else if (name === "MunkiServer") {
+      this.setState({
+        activeServer: name,
+        // url: second
+      })
+      this.forceUpdate()
+    } else if (name === "MockAPI") {
+      this.setState({
+        activeServer: name,
+        // url: "===> Third URL"
+      })
+      // this.forceUpdate()
+    }
+  }
 
   refreshData() {
-    console.log("onClick Button - Refreshed Data")
-    // reload page 
+  }
+
+  getServerNames() {
   }
   
   render() {
 
-    // console.log("Dashboard - API_URL", API_URL)
-    // console.log("Dashboard - USERNAME", USERNAME)
-    // console.log("Dashboard - PASSWORD", PASSWORD)
-    // console.log("Dashboard - auth", auth)
-    
+    console.log("1. Dashboard: activeServer", this.state.activeServer)
+    // console.log("1. Dashboard: url", this.state.url)
+
+    return (
+      <section className="dashboardv2">
+        <div className="dashboardv2__container">
+
+          <div className="header">
+            {/* <Menu>
+              <Menu.Item>
+                <Button primary>Sign up</Button>
+              </Menu.Item>
+              <Menu.Item>
+                <Button>Log-in</Button>
+              </Menu.Item>
+            </Menu> */}
+          </div>
+
+          <div className="dashboardv2__sidebar-container">
+            <Sidebar className="sidebar"/>          
+          </div>
+
+          {/* <Sidebar /> */}
+          <Tabs
+            activeServer={this.state.activeServer}
+            selectServerClick={this.selectServerClick}
+          />
+
+
+          {/* <Segment attached='bottom'>
+              <Client
+                server={this.state.activeServer}
+                // selectServerClick={this.selectServerClick}
+          />
+          </Segment> */}
+
+          {/* <Segment attached='bottom'>
+              <Client
+                server={this.state.activeServer}
+                // selectServerClick={this.selectServerClick}
+          />
+          </Segment> */}
+
+          {/* <Client className="dashboard__client"/> 
+          <Divider/> */}
+          
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Dashboard
+
+
     // const TabExampleDefaultActiveIndex = () => ( <Tab panes={panes} defaultActiveIndex={2} />)
     // export default TabExampleDefaultActiveIndex
     // Controlling Tab pane re-renders
@@ -137,55 +130,49 @@ class Dashboard extends Component {
     // https://stackoverflow.com/questions/49538086/how-can-i-make-sematic-ui-react-tab-responsive
     // Expandable Table https://codesandbox.io/s/expandable-table-in-semantic-ui-react-hu6yt
 
-    return (
-      <section className="dashboard">
-        <div className="dashboard__container">
+          // <Tab panes={panes} defaultActiveIndex={0} renderActiveOnly={false} />
+          //   <Tab.Pane>Tab 1 Content</Tab.Pane>
+          //   <Tab.Pane> <SrvInfo className="dashboardv2__srvinfo"/></Tab.Pane>
+          // <Tab/> 
+          // <Menu
+          //   items={[
+          //     { key: '1', name: 'Backup-Server-Mini', content: 'Backup Server Mini' },
+          //     { key: '2', name: 'Munki-Server', content: 'Munki Server' },
+          //     { key: '3', name: 'Mock-API', content: 'Mock API' },
+          //   ]}
+          //   pointing
+          //   tabular
+          // />
+            
+          // <Menu className=""
+          //   items={[
+          //     { key: '1', name: 'Backup-Server-Mini', content: 'Backup Server Mini' },
+          //     { key: '2', name: 'Munki-Server',       content: 'Munki Server' },
+          //     { key: '3', name: 'Mock-API',           content: 'Mock API' },
+          //   ]}
+          //   pointing
+          // />
 
-          {/* <Tab panes="panes" defaultActiveIndex={2} />
-            <Tab.Pane>Tab 1 Content</Tab.Pane>
-            <Tab.Pane>Tab 2 Content</Tab.Pane>
-            <Tab.Pane>Tab 3 Content</Tab.Pane>
-          <Tab/> */}
+          // <MenuHeading />
 
-          {/* <Menu
-            items={[
-              { key: '1', name: 'link-1', content: 'Link' },
-              { key: '2', name: 'link-2', content: 'Link' },
-              { key: '3', name: 'link-3', content: 'Link' },
-            ]}
-            pointing
-            tabular
-          /> */}
-          {/* <Menu
-            items={[
-              { key: 'l1', name: 'link-1', content: 'Link' },
-              { key: 'l2', name: 'link-2', content: 'Link' },
-              { key: 't1', name: 'text-1', content: 'Right text', position: 'right' },
-            ]}
-            pointing
-          /> */}
 
-          {/* <Refresh
-            className="dashboard__refresh"
-            refreshData={this.refreshData}/> */}
-            <Divider/>
-          <SrvInfo className="dashboard__srvinfo"/>
-            <Divider/>
-          <Devices className="dashboard__devices"/>
-            <Divider/>
-          <LicenseResources className="dashboard__licenseResources" />
-            <Divider/>
-          <Jukebox className="dashboard__jukebox" />
-            <Divider />         
-          <Pool className="dashboard__pool" />
-            <Divider />
-          <Jobs className="dashboard__jobs" />
-            <Divider/>
-          
-        </div>
-      </section>
-    )
-  }
-}
+            //           <Sidebar className="sidebarMaybe"
+            //   as={Menu}
+            //   icon='labeled'
+            //   vertical
+            //   visible
+            //   width='thin'
+            //   >
+            //   <Menu.Item as='a'>
+            //     <Icon name='home' /> Home
+            //   </Menu.Item>
+            //   <Menu.Item as='a'>
+            //     <Icon name='gamepad' /> Games
+            //   </Menu.Item>
+            //   <Menu.Item as='a'>
+            //     <Icon name='camera' /> Channels
+            //   </Menu.Item>
+            // </Sidebar >
+              
 
-export default Dashboard
+
