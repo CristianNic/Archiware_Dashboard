68d7f55712bfec8d212dc1c896b793270e9eaab1
Cristian <cristian.niculescu@gmail.com>
2021-12-04 01:37:16 -0800

    setting page routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // Redirect
-import Dashboard from "./pages/Dashboard";
-import Dashboardv2 from "./pages/Dashboardv2";
-// import Controls_Sandbox from "./components/Controls_Sandbox/Controls_Sandbox";
-import Settings from "./components/Settings/Settings";
+import StatusMainPage from "./pages/Dashboard";
+// import Archive from "./pages/ArchivePage/ArchivePage"
+// import Graphs from "./pages/GraphsPage/GraphsPage"
+// import Alerts from "./pages/AlertsPage/AlertsPage"
+import Settings from "./pages/SettingsPage/SettingsPage";
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<Switch>
-					<Route exact path="/" component={Dashboard} />
-					<Route exact path="/v2" component={Dashboardv2} />
-          <Route exact path="/base64auth" component={Settings} />
-          {/* <Route exact path="/base64auth" element={<Settings API_URL="www.sweet-test.com"/>} /> */}
-					{/* <Route exact path="/controls_sandbox" component={Controls_Sandbox} /> */}
+          <Route exact path="/status" component={StatusMainPage} />
+					{/* <Route exact path="/archive" component={Archive} /> */}
+					{/* <Route exact path="/graphs" component={Graphs} /> */}
+					{/* <Route exact path="/alerts" component={Alerts} /> */}
+					<Route exact path="/settings" component={Settings} />
 				</Switch>
 			</Router>
 		);
