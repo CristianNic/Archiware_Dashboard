57b21be552aa6d2aecc3d99ec8accccdb8bb3332
Cristian <cristian.niculescu@gmail.com>
2021-12-04 01:54:27 -0800

    Page refresh timer added. Specifically every table on the status page will refresh every 20min. Selecting the activeServer fucntion to display data from, and selecting the active Page functions are complete. The overall page layout is also complete.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,155 +1,94 @@
 import React, { Component } from "react";
-import axios from 'axios';
-import { API_URL, auth } from '../utils/Auth';
-import { Divider } from 'semantic-ui-react';
-// import Timer from '../components/Timer/Timer'
-// import CountDownTimer from '../components/CountDownTimer/CountDownTimer'
-// const hoursMinSecs = { hours: 0, minutes: 0, seconds: 7 }
-// import Refresh from "../components/Refresh/Refresh";
-import SrvInfo from "../components/Tables/SrvInfo/SrvInfo";
-// import Client from "../components/Tables/Client/Client";
-import Devices from "../components/Tables/Devices/Devices";
-// import LicenseResources from "../components/Tables/LicenseResources/LicenseResources";
-import Jukebox from "../components/Tables/Jukebox/Jukebox";
-// import Pool from "../components/Tables/Pool/Pool";
-// import Jobs from "../components/Tables/Jobs/Jobs";
-
-// const USERNAME = process.env.REACT_APP_USERNAME;
-// const PASSWORD = process.env.REACT_APP_PASSWORD;
+import Header from "../components/Header/Header"
+import ServerMenu from "../components/ServerMenu/ServerMenu"
+import Sidebar from "../components/Sidebar/Sidebar"
+import Layout from "../components/Layout/Layout"
 
 class Dashboard extends Component {
   
-  state = {
-    HomePage: [],
-    ArchivePlanNames: [],
-    ArchiveIndexNames: [],
-    ClientNames: [],  
-    ClientInfo: [],
-    isLoaded: true,
+  state = { 
+    activeServer: 'BackupServerMini',
+    activePage: 'status',              
+    updateIntervalMin: 20, 
+    refresh: true,                     
   }
 
   componentDidMount() {
-    // this.getHomePage()
+    this.myTimer = setInterval(() => {        
+       this.refresh()                    
+     }, (this.state.updateIntervalMin * 60000)) 
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.myTimer) 
+  }
+
+  refresh() {
+    this.setState({ refresh: !this.state.refresh})
   }
   
-  componentDidUpdate() {
+  selectServerClick = (name) => {
+    if (name === "BackupServerMini") {
+      this.setState({
+        activeServer: name
+      })
+    } else if (name === "MunkiServer") {
+      this.setState({
+        activeServer: name
+      })
+    } else if (name === "MockAPI") {
+      this.setState({
+        activeServer: name
+      })
+    }
   }
 
-  getHomePage() {  
-    axios
-      .get(`${API_URL}/`, auth)
-      .then((response) => {
-        console.log("getHomePage()", response)
-        this.setState({
-          HomePage: response.data.HomePage,
-        })
+  selectPageClick = (name) => {
+    if (name === "status") {
+      this.setState({
+        activePage: name
       })
-  }  
-
-  getArchivePlanNames() {  
-    axios
-      .get(`${API_URL}/archive/plans`)
-      .then((response) => {
-        // console.log(response)
-        this.setState({
-          ArchivePlanNames: response.data,
-        })
+    } else if (name === "archiveIndex") {
+      this.setState({
+        activePage: name
       })
-      .catch((error) => {
-        console.log('error:', error.response.data);
+    } else if (name === "graphs") {
+      this.setState({
+        activePage: name
       })
-  }
-
-  getArchiveIndexNames() {  
-    axios
-      .get(`${API_URL}/archive/indexes`)
-      .then((response) => {
-        // console.log("response", response.data)
-        this.setState({
-          ArchiveIndexNames: response.data,
-        })
+    } else if (name === "alerts") {
+      this.setState({
+        activePage: name
       })
-      .catch((error) => {
-        console.log('error:', error.response.data);
+    } else if (name === "settings") {
+      this.setState({
+        activePage: name
       })
-  }
-
-  getClientNames() {
-    axios
-      .get(`${API_URL}/general/clients`)
-      .then((response) => {
-      //  console.log(response)
-        this.setState({
-          ClientNames: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log('error:', error.response.data);
-      })
-  }
-
-  getClientInfo(clientID) {
-    axios
-      .get(`${API_URL}/general/clients/${clientID}`)  // jellyfish
-      .then((response) => {
-      //  console.log(response)
-        this.setState({
-          ClientInfo: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log('error:', error.response.data);
-      })
-  }
-
-  // countDown() {
-  //   // Remove one second, set state so a re-render happens.
-  //   const seconds = this.state.seconds - 1;
-  //   this.setState({
-  //     time: this.secondsToTime(seconds),
-  //     seconds: seconds,
-  //   });
-    
-  //   // Check if we're at zero.
-  //   if (seconds == 0) { 
-  //     clearInterval(this.timer);
-  //   }
-  // }
-
-  refreshData() {
-    console.log("onClick Button - Refreshed Data")
-    // reload page 
+    }
   }
   
   render() {
-
-    // console.log("Dashboard - API_URL", API_URL)
-    // console.log("Dashboard - USERNAME", USERNAME)
-    // console.log("Dashboard - PASSWORD", PASSWORD)
-    // console.log("Dashboard - auth", auth)
+    
+    const { activeServer, activePage, refresh } = this.state
     
     return (
-      <section className="dashboard">
-        <div className="dashboard__container">    
-          {/* <Refresh
-            className="dashboard__refresh"
-            refreshData={this.refreshData}/> */}
-            <Divider/>
-          {/* <SrvInfo className="dashboard__srvinfo"/>
-            <Divider/> */}
-          {/* <Client className="dashboard__client"/>
-            <Divider/> */}
-          <Devices className="dashboard__devices"/>
-            <Divider/>
-          {/* <LicenseResources className="dashboard__licenseResources" />
-            <Divider/> */}
-          <Jukebox className="dashboard__jukebox" />
-            <Divider />         
-          {/* <Pool className="dashboard__pool" />
-            <Divider /> */}
-          {/* <Jobs className="dashboard__jobs" />
-            <Divider/> */}
-          
+      <section className="dash">
+        <div className="dash__container">
+          <Header />
+          <ServerMenu
+            activeServer={activeServer} 
+            selectServerClick={this.selectServerClick}
+          />
+          <div className="dash__main">
+            <Sidebar
+              activeServer={activeServer}
+              activePage={activePage}  
+              selectPageClick={this.selectPageClick} />
+            <Layout
+              activeServer={activeServer}
+              refresh={refresh}
+            />
+          </div>
         </div>
       </section>
     )
