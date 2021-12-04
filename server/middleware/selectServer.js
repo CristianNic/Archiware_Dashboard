3af3f3e4cda8116b7e27a23431b489cd8eb414ef
Cristian <cristian.niculescu@gmail.com>
2021-12-04 02:14:07 -0800

    Basic Auth encodedToken is pulled out to be used later where headers require additional parameters such as in the jobs and jukebox routes

--- server/middleware/selectServer.js
+++ server/middleware/selectServer.js
@@ -1,18 +1,26 @@
 require("dotenv").config();
 
-const { authBSMini, authMS, authMA } = require("../auth")
+const {
+  authBSMini, authMS, authMA,
+  encodedTokenBSMini, encodedTokenMS, encodedTokenMA
+} = require("../auth")
 
 const selectServer = (req, res, next) => {
   if (req.headers.server === "BackupServerMini") {
-    res.locals.server = process.env.API_BackupServerMini;
-    res.locals.auth = authBSMini
+		res.locals.server = process.env.API_BackupServerMini;
+		res.locals.auth = authBSMini;
+    res.locals.encodedToken = encodedTokenBSMini;
+    
 	} else if (req.headers.server === "MunkiServer") {
     res.locals.server = process.env.API_Munki;
     res.locals.auth = authMS
+    res.locals.encodedToken = encodedTokenMS;
+
   } else if (req.headers.server === "MockAPI") {
     res.locals.server = process.env.API_MockAPI;
     res.locals.auth = authMA
-	}
+    res.locals.encodedToken = encodedTokenMA;
+  }
   next()
 }
 
