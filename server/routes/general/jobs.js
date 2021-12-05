b19d14019cf910bc5e41efe1ad3acc6cc0db9125
Cristian <cristian.niculescu@gmail.com>
2021-12-04 22:25:43 -0800

    removed console.logs

--- server/routes/general/jobs.js
+++ server/routes/general/jobs.js
@@ -8,18 +8,15 @@ router.route("/").get((req, res) => {
 	axios(`${server}/general/jobs/`, {
 		headers: {
 			Authorization: "Basic " + encodedToken,
-			lastdays: 2, // default 1 day // if string passed --> parseInt(req.params.lastdays, 10)
+      lastdays: 2,
+      // default is 1 day 
+      // pass a string from params with parseInt(req.params.lastdays, 10)
 			filter: "all",
 		},
 	})
-		.then(function (response) {
-			// console.log(`RESPONSE ===> ${req.params.lastdays}`, response.data);
-			// console.log(
-			// 	`RESPONSE Days ===> ${parseInt(req.params.lastdays, 10)}`,
-			// 	response.data
-			// );
-			res.json(response.data);
-		})
+    .then(function (response) {
+      res.json(response.data);
+    })
 		.catch(function (error) {
 			if (error.response) {
 				res.json({
@@ -48,7 +45,6 @@ router.route("/:jobID").get((req, res) => {
 		},
 	})
 		.then(function (response) {
-			// console.log(`RESPONSE ===> ${req.params.jobID}`, response.data);
 			res.json(response.data);
 		})
 		.catch(function (error) {
@@ -61,10 +57,8 @@ router.route("/:jobID").get((req, res) => {
 					responseData: error.response.data,
 				});
 			} else if (error.request) {
-				// The request was made but no response was received
 				console.log(error.request);
 			} else {
-				// Something happened in setting up the request that triggered an Error
 				console.log("Error", error.message);
 			}
 		});
@@ -76,12 +70,11 @@ router.route("/:jobID/protocol").get((req, res) => {
   axios(`${server}/general/jobs/${req.params.jobID}/`, {
 		headers: {
 			Authorization: "Basic " + encodedToken,
-			format: "json", // xml, path
+			format: "json", // can also be: xml, path
 			// filename: "string",
 		},
 	})
 		.then(function (response) {
-			// console.log(`RESPONSE Days ===> ${req.params.jobID}`, response.data);
 			res.json(response.data);
 		})
 		.catch(function (error) {
@@ -94,10 +87,8 @@ router.route("/:jobID/protocol").get((req, res) => {
 					responseData: error.response.data,
 				});
 			} else if (error.request) {
-				// The request was made but no response was received
 				console.log(error.request);
 			} else {
-				// Something happened in setting up the request that triggered an Error
 				console.log("Error", error.message);
 			}
 		});
@@ -112,7 +103,6 @@ router.route("/:jobID/report").get((req, res) => {
 		},
 	})
 		.then(function (response) {
-			// console.log(`RESPONSE Days ===> ${req.params.jobID}`, response.data);
 			res.json(response.data);
 		})
 		.catch(function (error) {
@@ -125,10 +115,8 @@ router.route("/:jobID/report").get((req, res) => {
 					responseData: error.response.data,
 				});
 			} else if (error.request) {
-				// The request was made but no response was received
 				console.log(error.request);
 			} else {
-				// Something happened in setting up the request that triggered an Error
 				console.log("Error", error.message);
 			}
 		});
