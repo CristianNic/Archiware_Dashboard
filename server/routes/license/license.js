c72650f08b598229cf5b28a9c65a6478331af85b
Cristian <cristian.niculescu@gmail.com>
2021-12-04 22:27:50 -0800

    moved license routes to their own file

---
+++ server/routes/license/license.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+// GET LicenseResourceNames
+router.route("/").get((req, res) => {
+  const { server, auth } = res.locals;
+	axios(`${server}/license/resources`, auth)
+		.then(function (response) {
+			res.json(response.data);
+		})
+		.catch(function (error) {
+			if (error.response) {
+				res.json({
+					resource: "Not Found",
+					headers: error.response.headers,
+					status: error.response.status,
+					statusText: error.response.statusText,
+					responseData: error.response.data,
+				});
+			} else if (error.request) {
+				// The request was made but no response was received
+				console.log(error.request);
+			} else {
+				// Something happened in setting up the request that triggered an Error
+				console.log("Error", error.message);
+			}
+		});
+});
+
+// GET LicenseResourceInfo
+router.route("/:resourceID").get((req, res) => {
+  const { server, auth } = res.locals;
+  axios(`${server}/license/resources/${req.params.resourceID}`, auth)
+		.then(function (response) {
+			res.json(response.data);
+		})
+		.catch(function (error) {
+			if (error.response) {
+				res.json({
+					resource: "Not Found",
+					headers: error.response.headers,
+					status: error.response.status,
+					statusText: error.response.statusText,
+					responseData: error.response.data,
+				});	
+			} else if (error.request) {
+				console.log(error.request);
+			} else {
+				console.log("Error", error.message);
+			}
+		});
+});
+
+module.exports = router;
+
