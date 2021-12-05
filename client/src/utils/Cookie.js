9e51227f50684bcb891d4125b39199f3e01efdee
Cristian <cristian.niculescu@gmail.com>
2021-12-04 22:46:53 -0800

    cookie is almost ready, outline is good, need to test if I go with this method of keeping track of notifications sent out.

--- client/src/utils/Cookie.js
+++ client/src/utils/Cookie.js
@@ -1,12 +1,22 @@
 import cookie from "react-cookie";
 
-export function setCookie(minutes) {
-	let d = new Date();
-	d.setTime(d.getTime() + minutes * 60 * 1000);
+// TODO: Log when an email/slack notification was sent. The time can be
+// retrieved and loaded into state. If the React client is restarted the 
+// cookie will remember that an email was sent and won't send another.
+// eg. Job warnings are sent once on slack as soon as they happen and 
+// maybe emailed once more at the start of each day, as per client request.
 
-	cookie.set("onboarded", true, { path: "/", expires: d });
+export function setCookie(jobID, minutes) {
+  const sent = Date.now();
+  const date = new Date();
+  // cookie expires after a set interval to send out another notification. 
+  date.setTime(date.getTime() + minutes * 60 * 1000);
+  
+  cookie.set("notification", true, {
+    job: jobID,
+    sent: sent,
+    expires: date
+  });
 }
 
-// for example sending an email/notification can be logged in a cookie
-// and loaded into state. If the react client is restarted the cookie will
-// remember that an email was sent and won't send another
+
