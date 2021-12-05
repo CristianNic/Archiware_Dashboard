c2b209f10927d615651fa9da0abca943793387c2
Cristian <cristian.niculescu@gmail.com>
2021-12-04 22:26:57 -0800

    removed logs, added longer error catch

--- server/routes/general/clients.js
+++ server/routes/general/clients.js
@@ -4,20 +4,32 @@ const axios = require("axios");
 
 // GET ClientNames
 router.route("/").get((req, res) => {
-  console.log("Client res.locals.server ---> ", res.locals.server);
   const { server, auth } = res.locals;
   axios(`${server}/general/clients`, auth)
 		.then(function (response) {
 			res.json(response.data);
 		})
 		.catch(function (error) {
-			console.log(error);
+			if (error.response) {
+				res.json({
+					resource: "Not Found",
+					headers: error.response.headers,
+					status: error.response.status,
+					statusText: error.response.statusText,
+					responseData: error.response.data,
+				});
+			} else if (error.request) {
+				// The request was made but no response was received
+				console.log(error.request);
+			} else {
+				// Something happened in setting up the request that triggered an Error
+				console.log("Error", error.message);
+			}
 		});
 });
 
 // GET ClientInfo
 router.route("/:clientID").get((req, res) => {
-  console.log("Client res.locals.server---> ", res.locals.server);
   const { server, auth } = res.locals
 	axios(`${server}/general/clients/${req.params.clientID}`, auth)
 		.then(function (response) {
@@ -33,10 +45,8 @@ router.route("/:clientID").get((req, res) => {
 					responseData: error.response.data,
 				});		
 			} else if (error.request) {
-				// The request was made but no response was received
 				console.log(error.request);
 			} else {
-				// Something happened in setting up the request that triggered an Error
 				console.log("Error", error.message);
 			}
 		});
