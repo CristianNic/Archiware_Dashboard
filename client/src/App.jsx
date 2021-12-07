70f1491bb38b38c6011f13967713e5ede403916b
Cristian <cristian.niculescu@gmail.com>
2021-12-06 22:20:14 -0800

    Simplified routes to one main entry point. This way it can hold state in one place for the rest of the components.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // Redirect
-import StatusMainPage from "./pages/Dashboard";
-// import Archive from "./pages/ArchivePage/ArchivePage"
-// import Graphs from "./pages/GraphsPage/GraphsPage"
-// import Alerts from "./pages/AlertsPage/AlertsPage"
-import Settings from "./pages/SettingsPage/SettingsPage";
+import MainPage from "./pages/MainPage";
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<Switch>
-          <Route exact path="/status" component={StatusMainPage} />
-					{/* <Route exact path="/archive" component={Archive} /> */}
-					{/* <Route exact path="/graphs" component={Graphs} /> */}
-					{/* <Route exact path="/alerts" component={Alerts} /> */}
-					<Route exact path="/settings" component={Settings} />
+          <Route exact path="/" component={MainPage} />
 				</Switch>
 			</Router>
 		);
