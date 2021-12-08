67ab13e71242579ba9c0b4984d079add85c719d0
Cristian <cristian.niculescu@gmail.com>
2021-12-07 23:04:34 -0800

    table sorting is removed because jobs with warnings remain at the top. Added an empty row displaying None when api call returns that there are no jobs running to report on.

--- client/src/components/Tables/Jobs/Jobs.jsx
+++ client/src/components/Tables/Jobs/Jobs.jsx
@@ -27,71 +27,94 @@ class Jobs extends Component {
     // "completed, failed, pending, running or warning" return "Internal Server Error:"
     const getJobNames = await axios.get(`${API_URL}/general/jobs/`, server(this.props.activeServer))
 
-    const jobIDs = getJobNames.data.jobs.map(job => job.ID) 
+    const jobIDs = getJobNames.data.jobs.map(job => job.ID)
 
-    const getJobProtocolPromises = []
-    jobIDs.forEach((id) => {
-      getJobProtocolPromises.push(axios.get(`${API_URL}/general/jobs/${id}/protocol`, server(this.props.activeServer)))
-    })
-    const getJobProtocol = await Promise.all(getJobProtocolPromises)
+    if (Object.keys(jobIDs).length === 0) {
+      const noJobs = [{
+        jobID: "None",
+        label: [],
+        result: [],
+        status: [],
+        report: []
+      }]
+      this.setState({
+        jobInfoTableSorted: noJobs
+      })
+    } else {
 
-    const result = []  
-    const status = [] 
-    getJobProtocol.forEach((job) => {
-      result.push(job.data.completion)
-      status.push(job.data.status)
-    })
+      const getJobInfoPromises = []
+      jobIDs.forEach((id) => {
+        getJobInfoPromises.push(axios.get(`${API_URL}/general/jobs/${id}`, server(this.props.activeServer)))
+      })
+      const getJobInfo = await Promise.all(getJobInfoPromises)
 
-    const getJobReportPromises = []
-    jobIDs.forEach((id) => {
-      getJobReportPromises.push(axios.get(`${API_URL}/general/jobs/${id}/report`, server(this.props.activeServer)))
-    })
-    const getJobReport = await Promise.all(getJobReportPromises)
+      const label = getJobInfo.map(info => info.data.label)
 
-    const reports = getJobReport.map(report => report.data)
+      const getJobProtocolPromises = []
+      jobIDs.forEach((id) => {
+        getJobProtocolPromises.push(axios.get(`${API_URL}/general/jobs/${id}/protocol`, server(this.props.activeServer)))
+      })
+      const getJobProtocol = await Promise.all(getJobProtocolPromises)
 
-    const formattedReports = []
-    reports.forEach((report) => {
-      if (report === "<empty>") {
-        formattedReports.push("empty") 
-      } else {
-        formattedReports.push(report)
+      const result = []
+      const status = []
+      getJobProtocol.forEach((job) => {
+        result.push(job.data.completion)
+        status.push(job.data.status)
+      })
+
+      const getJobReportPromises = []
+      jobIDs.forEach((id) => {
+        getJobReportPromises.push(axios.get(`${API_URL}/general/jobs/${id}/report`, server(this.props.activeServer)))
+      })
+      const getJobReport = await Promise.all(getJobReportPromises)
+
+      const reports = getJobReport.map(report => report.data)
+
+      const formattedReports = []
+      reports.forEach((report) => {
+        if (report === "<empty>") {
+          formattedReports.push("empty")
+        } else {
+          formattedReports.push(report)
+        }
+      })
+      const jobInfoTable = []
+      for (let i = 0; i < jobIDs.length; i++) {
+        const obj = {
+          jobID: jobIDs[i],
+          label: label[i],
+          result: result[i],
+          status: status[i],
+          report: formattedReports[i]
+        }
+        jobInfoTable.push(obj)
       }
-    }) 
-    const jobInfoTable = []
-    for (let i = 0; i < jobIDs.length; i++) {
-      const obj = {
-        jobID: jobIDs[i],
-        result: result[i],
-        status: status[i],
-        report: formattedReports[i]
-      }
-      jobInfoTable.push(obj)
+
+      // const failure = []
+      // const warning = []
+      // const success = []
+      // const rest = []
+
+      // jobInfoTable.map((job) => {
+      //   if (job.result === "failure") {
+      //     failure.push(job)
+      //   } else if (job.result === "warning") {
+      //     warning.push(job)
+      //   } else if (job.result === "success") {
+      //     success.push(job)
+      //   } else {
+      //     rest.push(job)
+      //   }
+      //   return false
+      // })
+      // const jobInfoTableSorted = [...failure, ...warning, ...success, ...rest]
+
+      this.setState({
+        jobInfoTableSorted: jobInfoTable
+      })
     }
-
-    const failure = []
-    const warning = []
-    const success = []
-    const rest = []
-
-    jobInfoTable.map((job) => {
-      if (job.result === "failure") {
-        failure.push(job)
-      } else if (job.result === "warning") {
-        warning.push(job)
-      } else if (job.result === "success") {
-        success.push(job)
-      } else {
-        rest.push(job)
-      }
-      return false
-    })
-    const jobInfoTableSorted = [...failure, ...warning, ...success, ...rest]
-
-    this.setState({
-      jobInfoTableSorted: jobInfoTableSorted
-    })
-  }  
+  }
 
   render() {
     
@@ -105,6 +128,7 @@ class Jobs extends Component {
             <Table.Header>
               <Table.Row>
                 <Table.HeaderCell>Job{"\u00A0"}ID</Table.HeaderCell>  
+                <Table.HeaderCell>Label</Table.HeaderCell>
                 <Table.HeaderCell>Result</Table.HeaderCell>
                 <Table.HeaderCell>Status</Table.HeaderCell>
                 <Table.HeaderCell>Report</Table.HeaderCell>
@@ -117,12 +141,22 @@ class Jobs extends Component {
                   <Table.Cell>loading...</Table.Cell>
                   <Table.Cell>loading...</Table.Cell>
                   <Table.Cell>loading...</Table.Cell>
+                  <Table.Cell>loading...</Table.Cell>
                 </Table.Row>)
                 :
                 (jobInfoTableSorted.map(job =>
-                  job.result === 'success' ?
+                  job.jobID === 'None' ?
+                    ( <Table.Row key={job.jobID}>
+                        <Table.Cell className="jobs__text-green">{job.jobID}</Table.Cell>
+                        <Table.Cell>{job.label}</Table.Cell>
+                        <Table.Cell>{job.result}</Table.Cell>
+                        <Table.Cell>{job.status}</Table.Cell>
+                        <Table.Cell className>{job.report}</Table.Cell>
+                    </Table.Row>)
+                  : job.result === 'success' ?
                     ( <Table.Row positive key={job.jobID}>
                         <Table.Cell>{job.jobID}</Table.Cell>
+                        <Table.Cell>{job.label}</Table.Cell>
                         <Table.Cell>{job.result}</Table.Cell>
                         <Table.Cell>{job.status}</Table.Cell>
                         <Table.Cell className>{job.report}</Table.Cell>
@@ -130,18 +164,18 @@ class Jobs extends Component {
                   : job.result === 'warning' ?
                     ( <Table.Row warning key={job.jobID}>
                         <Table.Cell>{job.jobID}</Table.Cell>
+                        <Table.Cell>{job.label}</Table.Cell>
                         <Table.Cell>{job.result}</Table.Cell>
                         <Table.Cell>{job.status}</Table.Cell>
                         <Table.Cell>{job.report}</Table.Cell>
                       </Table.Row>)
-                  :
-                    ( <Table.Row negative key={job.jobID}>
+                  : ( <Table.Row negative key={job.jobID}>
                         <Table.Cell>{job.jobID}</Table.Cell>
+                        <Table.Cell>{job.label}</Table.Cell>
                         <Table.Cell>{job.result}</Table.Cell>
                         <Table.Cell>{job.status}</Table.Cell>
                         <Table.Cell>{job.report}</Table.Cell>
-                      </Table.Row>
-                    )
+                      </Table.Row>)
                 ))
               }
             </Table.Body>
